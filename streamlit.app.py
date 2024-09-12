5b444db166679fca13f47dacb020cb0d7d71b4aa
oxnvrgo <m.gelo.monserrat@gmail.com>
2024-09-12 08:39:02 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -27,7 +27,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{ingredients_string}")
         fv_df = st.dataframe( data=fruityvice_response.json(), use_container_width=True )
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
